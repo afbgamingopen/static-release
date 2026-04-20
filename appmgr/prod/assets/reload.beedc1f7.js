3da1bbe8d097a6cfa1b6c5d476ae4f481172fd1a
auto release <release@gmail.com>
2026-04-20 11:31:08 +0800

    release: dist -> appmgr/prod

---
+++ appmgr/prod/assets/reload.beedc1f7.js
@@ -0,0 +1,1 @@
+import{a,o as e,c as s}from"./index.e1bbc841.js";const o=Object.assign({name:"Reload"},{__name:"reload",setup:o=>(a().go(-1),(a,o)=>(e(),s("div")))});export{o as default};
--- appmgr/prod/assets/validator.6e0aa035.js
+++
@@ -1,1 +0,0 @@
-import{S as s}from"./index.5d3f2638.js";const i=i=>["",...s].includes(i);export{i};
