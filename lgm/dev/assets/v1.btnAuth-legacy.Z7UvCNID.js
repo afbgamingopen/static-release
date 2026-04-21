012f0fd3bc586ae25eadbc8a562cca015deeb7a3
auto release <release@gmail.com>
2026-04-21 18:29:01 +0800

    release: dist -> lgm/dev

--- lgm/dev/assets/v1.btnAuth-legacy.Z7UvCNID.js
+++
@@ -1,1 +0,0 @@
-System.register(["./v1.index-legacy.5cTPMDwA.js"],function(t,e){"use strict";var n,r;return{setters:[function(t){n=t.q,r=t.j}],execute:function(){t("u",function(){return n().meta.btns||r({})})}}});
