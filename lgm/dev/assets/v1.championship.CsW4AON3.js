50dd0d94aa143b9c2a71ee3708d4d56d0a391299
auto release <release@gmail.com>
2026-04-23 14:32:12 +0800

    release: dist -> lgm/dev

--- lgm/dev/assets/v1.championship.CsW4AON3.js
+++
@@ -1,1 +0,0 @@
-import{s as a}from"./v1.index.DAI9dEBJ.js";const t=t=>a({url:"/game/activity/championship/records",method:"post",data:t}),s=t=>a({url:"/game/activity/championship/results",method:"post",data:t});export{s as a,t as g};
