e719c5558611d75916abca53bcf938997889e3a4
auto release <release@gmail.com>
2026-04-23 15:32:57 +0800

    release: dist -> appmgr/prod

---
+++ appmgr/prod/assets/castArray.86142b6d.js
@@ -0,0 +1,1 @@
+import{aP as r}from"./index.e1ac7b79.js";function n(){if(!arguments.length)return[];var n=arguments[0];return r(n)?n:[n]}export{n as c};
--- appmgr/prod/assets/validator.67995d25.js
+++
@@ -1,1 +0,0 @@
-import{S as s}from"./index.339ecde7.js";const i=i=>["",...s].includes(i);export{i};
