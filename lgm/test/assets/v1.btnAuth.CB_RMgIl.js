384717b541b23073080abed4200e7d9f03df3192
auto release <release@gmail.com>
2026-04-24 17:21:24 +0800

    release: dist -> lgm/test

--- lgm/test/assets/v1.btnAuth.CB_RMgIl.js
+++
@@ -1,1 +0,0 @@
-import{q as s,j as t}from"./v1.index.4OHZoMFi.js";const a=()=>s().meta.btns||t({});export{a as u};
---
+++ lgm/test/assets/v1.btnAuth.UNqTw-ob.js
@@ -0,0 +1,1 @@
+import{q as s,j as t}from"./v1.index.3Rky_vBN.js";const a=()=>s().meta.btns||t({});export{a as u};
