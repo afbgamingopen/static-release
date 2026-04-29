dfed7f7ff3d067489dc074d9d70f528f545717ff
auto release <release@gmail.com>
2026-04-29 11:44:15 +0800

    release: dist -> lgm/test

---
+++ lgm/test/assets/v1.btnAuth.BiOtqubr.js
@@ -0,0 +1,1 @@
+import{q as s,j as t}from"./v1.index.VvT97_D5.js";const a=()=>s().meta.btns||t({});export{a as u};
--- lgm/test/assets/v1.btnAuth.Bu3YdQa5.js
+++
@@ -1,1 +0,0 @@
-import{q as s,j as t}from"./v1.index.q85Dtu1N.js";const a=()=>s().meta.btns||t({});export{a as u};
