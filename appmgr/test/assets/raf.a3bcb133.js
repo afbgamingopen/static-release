c6f7faeac07629fef0910fdee146db15740ef296
auto release <release@gmail.com>
2026-04-30 15:40:46 +0800

    release: dist -> appmgr/test

---
+++ appmgr/test/assets/raf.a3bcb133.js
@@ -0,0 +1,1 @@
+import{av as e}from"./index.5a634ce6.js";const a=a=>e?window.requestAnimationFrame(a):setTimeout(a,16),i=a=>e?window.cancelAnimationFrame(a):clearTimeout(a);export{i as c,a as r};
--- appmgr/test/assets/validator.0abf12ed.js
+++
@@ -1,1 +0,0 @@
-import{S as s}from"./index.b5a8194a.js";const i=i=>["",...s].includes(i);export{i};
