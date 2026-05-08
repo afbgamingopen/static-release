87690c2e8636a3122a7496d313e190c91a76a75d
auto release <release@gmail.com>
2026-05-08 16:30:54 +0800

    release: dist -> appmgr/test

---
+++ appmgr/test/assets/isArrayLikeObject.e1c3d741.js
@@ -0,0 +1,1 @@
+import{s,o as a,i as r}from"./_flatRest.dd5fe9e2.js";import{ba as t,a$ as o}from"./index.d3da9167.js";function i(t,o){return s(a(t,o,r),t+"")}function n(s){return t(s)&&o(s)}export{i as b,n as i};
