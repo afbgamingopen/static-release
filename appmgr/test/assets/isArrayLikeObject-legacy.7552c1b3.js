2ac96b3b1cfce15a6af0a50bc0c802466afc4d23
auto release <release@gmail.com>
2026-05-12 18:07:19 +0800

    release: dist -> appmgr/test

--- appmgr/test/assets/isArrayLikeObject-legacy.7552c1b3.js
+++
@@ -1,1 +0,0 @@
-System.register(["./_flatRest-legacy.d178bbb2.js","./index-legacy.e5af44fc.js"],function(t,e){"use strict";var n,i,r,u,c;return{setters:[function(t){n=t.s,i=t.o,r=t.i},function(t){u=t.ba,c=t.a$}],execute:function(){t({b:function(t,e){return n(i(t,e,r),t+"")},i:function(t){return u(t)&&c(t)}})}}});
---
+++ appmgr/test/assets/isArrayLikeObject.683f15b3.js
@@ -0,0 +1,1 @@
+import{s,o as a,i as r}from"./_flatRest.9a81316d.js";import{ba as t,a$ as o}from"./index.52457dee.js";function i(t,o){return s(a(t,o,r),t+"")}function n(s){return t(s)&&o(s)}export{i as b,n as i};
