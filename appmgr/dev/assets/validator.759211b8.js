b4820000ca5eb1ff76f3d6980ed51d689da21d0d
auto release <release@gmail.com>
2026-05-12 17:10:44 +0800

    release: dist -> appmgr/dev

--- appmgr/dev/assets/validator.759211b8.js
+++
@@ -1,1 +0,0 @@
-import{S as s}from"./index.154bdfcc.js";const i=i=>["",...s].includes(i);export{i};
---
+++ appmgr/dev/assets/validator.7fef96b6.js
@@ -0,0 +1,1 @@
+import{S as s}from"./index.0ff2bbe9.js";const i=i=>["",...s].includes(i);export{i};
