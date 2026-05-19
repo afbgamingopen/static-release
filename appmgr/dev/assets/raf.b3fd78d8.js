275319192ec271f5d1893489aefdcb54f16fc154
auto release <release@gmail.com>
2026-05-19 10:40:53 +0800

    release: dist -> appmgr/dev

---
+++ appmgr/dev/assets/raf.b3fd78d8.js
@@ -0,0 +1,1 @@
+import{av as e}from"./index.c9f24efa.js";const a=a=>e?window.requestAnimationFrame(a):setTimeout(a,16),i=a=>e?window.cancelAnimationFrame(a):clearTimeout(a);export{i as c,a as r};
---
+++ appmgr/dev/assets/useBtnAuth.f7dbd3b0.js
@@ -0,0 +1,1 @@
+import{s as t,j as e}from"./index.c9f24efa.js";const s=(s=!1)=>{const n=t();if(s){return n.matched[n.matched.length-2].meta.btns||e({})}return n.meta.btns||e({})};export{s as u};
