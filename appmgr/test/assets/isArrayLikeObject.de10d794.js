40c3a7448aa79e27a3d925f40e889424114175f1
auto release <release@gmail.com>
2026-05-21 14:38:21 +0800

    release: dist -> appmgr/test

--- appmgr/test/assets/isArrayLikeObject.de10d794.js
+++
@@ -1,1 +0,0 @@
-import{s,o as a,i as r}from"./_flatRest.f4202351.js";import{ba as t,a$ as o}from"./index.06c61500.js";function i(t,o){return s(a(t,o,r),t+"")}function n(s){return t(s)&&o(s)}export{i as b,n as i};
--- appmgr/test/assets/validator.39e02c8f.js
+++
@@ -1,1 +0,0 @@
-import{S as s}from"./index.06c61500.js";const i=i=>["",...s].includes(i);export{i};
