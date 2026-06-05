5dc9a3a180768990472f600fda589cf77fc52baa
auto release <release@gmail.com>
2026-06-05 10:06:16 +0800

    release: dist -> appmgr/prod

--- appmgr/prod/assets/castArray.5d40a48a.js
+++
@@ -1,1 +0,0 @@
-import{aP as r}from"./index.02f863df.js";function n(){if(!arguments.length)return[];var n=arguments[0];return r(n)?n:[n]}export{n as c};
--- appmgr/prod/assets/validator.5bc5dded.js
+++
@@ -1,1 +0,0 @@
-import{S as s}from"./index.02f863df.js";const i=i=>["",...s].includes(i);export{i};
