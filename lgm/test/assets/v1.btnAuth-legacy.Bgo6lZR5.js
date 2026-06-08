e7adfc427f3908e14696bf2541bcf5c85c97b988
auto release <release@gmail.com>
2026-06-08 14:52:14 +0800

    release: dist -> lgm/test

---
+++ lgm/test/assets/v1.btnAuth-legacy.Bgo6lZR5.js
@@ -0,0 +1,1 @@
+System.register(["./v1.index-legacy.B1gSyaZ8.js"],function(t,e){"use strict";var n,r;return{setters:[function(t){n=t.q,r=t.j}],execute:function(){t("u",function(){return n().meta.btns||r({})})}}});
