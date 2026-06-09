f446958483f5732b7dcb71be208678405c8ac815
auto release <release@gmail.com>
2026-06-09 11:06:24 +0800

    release: dist -> lgm/test

--- lgm/test/assets/v1.wealthSafetyNet.CSyj80EM.js
+++
@@ -1,1 +0,0 @@
-import{s as t}from"./v1.index.CuR3pdW6.js";const a=a=>t({url:"/game/activity/wealthSafetyNet/events",method:"post",data:a}),e=a=>t({url:"/game/activity/wealthSafetyNet/statDay",method:"post",data:a});export{e as a,a as g};
