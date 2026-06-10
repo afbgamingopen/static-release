4df41a940f3ec6e47b21014f0449a88178920915
auto release <release@gmail.com>
2026-06-10 12:38:20 +0800

    release: dist -> lgm/dev

---
+++ lgm/dev/assets/v1.btnAuth-legacy.CiSPJevs.js
@@ -0,0 +1,1 @@
+System.register(["./v1.vendor-legacy.CcvII4x2.js"],function(t,e){"use strict";var n,r;return{setters:[function(t){n=t.bm,r=t.x}],execute:function(){t("u",function(){return n().meta.btns||r({})})}}});
--- lgm/dev/assets/v1.btnAuth.BjY3SQQc.js
+++
@@ -1,1 +0,0 @@
-import{aD as s,p as a}from"./v1.vue-vendor.CeSF0iwP.js";const o=()=>s().meta.btns||a({});export{o as u};
