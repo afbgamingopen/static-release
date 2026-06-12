700940f270651e815efbc0e307083f094229c8e1
auto release <release@gmail.com>
2026-06-12 12:03:16 +0800

    release: dist -> appmgr/test

--- appmgr/test/assets/castArray-legacy.cd380f49.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy.fa6a4e56.js"],function(e,t){"use strict";var n;return{setters:[function(e){n=e.aP}],execute:function(){e("c",function(){if(!arguments.length)return[];var e=arguments[0];return n(e)?e:[e]})}}});
