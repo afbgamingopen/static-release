4dd881636a612e9843b21233845e01d8d869b4bc
auto release <release@gmail.com>
2026-06-26 10:30:56 +0800

    release: dist -> appmgr/test

--- appmgr/test/assets/castArray-legacy.36596bc7.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy.1a636b4b.js"],function(e,t){"use strict";var n;return{setters:[function(e){n=e.aJ}],execute:function(){e("c",function(){if(!arguments.length)return[];var e=arguments[0];return n(e)?e:[e]})}}});
--- appmgr/test/assets/isArrayLikeObject.fa7b8be5.js
+++
@@ -1,1 +0,0 @@
-import{cQ as a,cR as s,cS as n,aZ as r,aO as t}from"./index.61e44e9e.js";function c(r,t){return a(s(r,t,n),r+"")}function i(a){return r(a)&&t(a)}export{c as b,i};
