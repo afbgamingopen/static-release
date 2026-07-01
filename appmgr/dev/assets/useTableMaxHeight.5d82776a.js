d448af374c6b203a542aa4e78e9365ad7df464b3
auto release <release@gmail.com>
2026-07-01 18:23:52 +0800

    release: dist -> appmgr/dev

---
+++ appmgr/dev/assets/useTableMaxHeight.5d82776a.js
@@ -0,0 +1,1 @@
+import{u as t,a as o}from"./index.07ffd3af.js";import{B as a}from"./index.91e26acd.js";function e(e,i={}){const{offsetBottom:m=72,minHeight:n=500}=i,{height:r}=t(),{top:s}=o(e,{windowScroll:!1});return{maxHeight:a(()=>Math.max(r.value-s.value-m,n))}}export{e as u};
