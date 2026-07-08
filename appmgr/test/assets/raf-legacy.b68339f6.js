a2677e7e54154631a06123cff0110fceb0c80641
auto release <release@gmail.com>
2026-07-08 15:16:51 +0800

    release: dist -> appmgr/test

--- appmgr/test/assets/raf-legacy.b68339f6.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy.3d7c8499.js"],function(e,n){"use strict";var t;return{setters:[function(e){t=e.aG}],execute:function(){e("r",function(e){return t?window.requestAnimationFrame(e):setTimeout(e,16)}),e("c",function(e){return t?window.cancelAnimationFrame(e):clearTimeout(e)})}}});
