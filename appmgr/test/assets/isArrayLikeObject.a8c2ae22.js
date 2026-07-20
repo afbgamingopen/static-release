c230f65adc1d8625be2df3386b78b436abf6e87e
auto release <release@gmail.com>
2026-07-20 15:33:06 +0800

    release: dist -> appmgr/test

--- appmgr/test/assets/isArrayLikeObject.a8c2ae22.js
+++
@@ -1,1 +0,0 @@
-import{cR as a,cS as s,cT as n,aY as r,aN as t}from"./index.187f4518.js";function c(r,t){return a(s(r,t,n),r+"")}function i(a){return r(a)&&t(a)}export{c as b,i};
---
+++ appmgr/test/assets/useBtnAuth.89fa07da.js
@@ -0,0 +1,1 @@
+import{s as t,i as e}from"./index.016bf235.js";const s=(s=!1)=>{const n=t();if(s){return n.matched[n.matched.length-2].meta.btns||e({})}return n.meta.btns||e({})};export{s as u};
