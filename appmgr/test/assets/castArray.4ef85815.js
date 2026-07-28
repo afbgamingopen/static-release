aca916aa1158c8818383781ac562a3a441f9b951
auto release <release@gmail.com>
2026-07-28 10:03:54 +0800

    release: dist -> appmgr/test

--- appmgr/test/assets/castArray.4ef85815.js
+++
@@ -1,1 +0,0 @@
-import{aI as r}from"./index.a1c24af0.js";function n(){if(!arguments.length)return[];var n=arguments[0];return r(n)?n:[n]}export{n as c};
---
+++ appmgr/test/assets/castArray.c5ff900f.js
@@ -0,0 +1,1 @@
+import{aI as r}from"./index.a9d6afa7.js";function n(){if(!arguments.length)return[];var n=arguments[0];return r(n)?n:[n]}export{n as c};
