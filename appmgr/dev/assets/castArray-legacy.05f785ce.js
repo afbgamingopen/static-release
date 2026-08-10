f935b7ebf89dbd31104fab1f9e07607095778c29
auto release <release@gmail.com>
2026-08-10 14:39:53 +0800

    release: dist -> appmgr/dev

--- appmgr/dev/assets/castArray-legacy.05f785ce.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy.7c7c533c.js"],function(e,t){"use strict";var n;return{setters:[function(e){n=e.aI}],execute:function(){e("c",function(){if(!arguments.length)return[];var e=arguments[0];return n(e)?e:[e]})}}});
---
+++ appmgr/dev/assets/isArrayLikeObject-legacy.97dc3391.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy.b315d4c0.js"],function(t,e){"use strict";var n,c,r,u,i;return{setters:[function(t){n=t.cR,c=t.cS,r=t.cT,u=t.aY,i=t.aN}],execute:function(){t({b:function(t,e){return n(c(t,e,r),t+"")},i:function(t){return u(t)&&i(t)}})}}});
