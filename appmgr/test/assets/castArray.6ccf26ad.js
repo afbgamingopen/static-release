beff15c85e7bc31796ec17098879a384a7f0c242
auto release <release@gmail.com>
2026-08-17 17:19:28 +0800

    release: dist -> appmgr/test

--- appmgr/test/assets/castArray.6ccf26ad.js
+++
@@ -1,1 +0,0 @@
-import{aI as r}from"./index.37e1c9dd.js";function n(){if(!arguments.length)return[];var n=arguments[0];return r(n)?n:[n]}export{n as c};
---
+++ appmgr/test/assets/raf.f81df8eb.js
@@ -0,0 +1,1 @@
+import{aF as e}from"./index.38650c53.js";const a=a=>e?window.requestAnimationFrame(a):setTimeout(a,16),i=a=>e?window.cancelAnimationFrame(a):clearTimeout(a);export{i as c,a as r};
