7d3d72a975426e6e720ae6a49329973dcd75fbc2
auto release <release@gmail.com>
2026-08-25 10:51:17 +0800

    release: dist -> appmgr/test

---
+++ appmgr/test/assets/castArray.5d68feff.js
@@ -0,0 +1,1 @@
+import{aI as r}from"./index.763f8c35.js";function n(){if(!arguments.length)return[];var n=arguments[0];return r(n)?n:[n]}export{n as c};
---
+++ appmgr/test/assets/isArrayLikeObject.10131f11.js
@@ -0,0 +1,1 @@
+import{cR as a,cS as s,cT as n,aY as r,aN as t}from"./index.763f8c35.js";function c(r,t){return a(s(r,t,n),r+"")}function i(a){return r(a)&&t(a)}export{c as b,i};
