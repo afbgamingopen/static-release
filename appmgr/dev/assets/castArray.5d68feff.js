4032c8a6a8e488d4a0a890ceb97fe89fcdb73a2a
auto release <release@gmail.com>
2026-08-25 14:49:43 +0800

    release: dist -> appmgr/dev

--- appmgr/dev/assets/castArray.5d68feff.js
+++
@@ -1,1 +0,0 @@
-import{aI as r}from"./index.763f8c35.js";function n(){if(!arguments.length)return[];var n=arguments[0];return r(n)?n:[n]}export{n as c};
---
+++ appmgr/dev/assets/castArray.e89b0f73.js
@@ -0,0 +1,1 @@
+import{aI as r}from"./index.e7a4b7a3.js";function n(){if(!arguments.length)return[];var n=arguments[0];return r(n)?n:[n]}export{n as c};
