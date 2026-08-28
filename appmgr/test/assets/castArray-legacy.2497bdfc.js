860b3bde6ed59935dab0b5524bf34fa11086ce56
Your Name <you@example.com>
2026-08-28 11:45:37 +0800

    release: dist -> appmgr/test

---
+++ appmgr/test/assets/castArray-legacy.2497bdfc.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy.4627ef80.js"],function(e,t){"use strict";var n;return{setters:[function(e){n=e.aJ}],execute:function(){e("c",function(){if(!arguments.length)return[];var e=arguments[0];return n(e)?e:[e]})}}});
