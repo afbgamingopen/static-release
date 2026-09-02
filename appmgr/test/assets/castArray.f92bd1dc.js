d54a77d0ba5084976cd3b8f4f25d8dbc1bafe182
Your Name <you@example.com>
2026-09-02 14:25:19 +0800

    release: dist -> appmgr/test

---
+++ appmgr/test/assets/castArray.f92bd1dc.js
@@ -0,0 +1,1 @@
+import{aJ as r}from"./index.9ca58a4a.js";function n(){if(!arguments.length)return[];var n=arguments[0];return r(n)?n:[n]}export{n as c};
--- appmgr/test/assets/strings.0ed781be.js
+++
@@ -1,1 +0,0 @@
-import{c_ as e}from"./index.b2df3819.js";const a=(e="")=>e.replace(/[|\\{}()[\]^$+*?.]/g,"\\$&").replace(/-/g,"\\x2d"),c=a=>e(a);export{c,a as e};
