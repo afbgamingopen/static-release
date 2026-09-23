7cc29f3c495559f496c8bca4753469c0417d1d9d
Your Name <you@example.com>
2026-09-23 11:58:49 +0800

    release: dist -> appmgr/test

---
+++ appmgr/test/assets/castArray-legacy.5f0ed5bd.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy.da2cb837.js"],function(e,t){"use strict";var n;return{setters:[function(e){n=e.aJ}],execute:function(){e("c",function(){if(!arguments.length)return[];var e=arguments[0];return n(e)?e:[e]})}}});
--- appmgr/test/assets/strings.be548a26.js
+++
@@ -1,1 +0,0 @@
-import{c_ as e}from"./index.9178c721.js";const a=(e="")=>e.replace(/[|\\{}()[\]^$+*?.]/g,"\\$&").replace(/-/g,"\\x2d"),c=a=>e(a);export{c,a as e};
